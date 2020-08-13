b7c1918f586f1002f59b564e11a83553a823735a
JOCh <jchicahuala@cybelesoft.com>
2020-08-13 18:59:23 -0300

    1er Creando encoder and destroy

--- EncoderH264/EncoderH264.cpp
+++ EncoderH264/EncoderH264.cpp
@@ -6,24 +6,26 @@
 #include "EncoderH264.h"
 
 #include <cstddef>
+#include <iostream>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
-
-// This is an example of an exported variable
-ENCODERH264_API int nEncoderH264 = 0;
-
-// This is an example of an exported function.
-ENCODERH264_API int fnEncoderH264(void)
-	{
-	return 0;
-	}
-
-	// This is the constructor of a class that has been exported.
-CEncoderH264::CEncoderH264()
-	{
-	return;
-	}
+//// This is an example of an exported variable
+//ENCODERH264_API int nEncoderH264 = 0;
+//
+//// This is an example of an exported function.
+//ENCODERH264_API int fnEncoderH264(void)
+//	{
+//	return 0;
+//	}
+//
+//// This is the constructor of a class that has been exported.
+//CEncoderH264::CEncoderH264()
+//	{
+//	return;
+//	}
 
 extern "C"
 	{
@@ -33,9 +35,169 @@ extern "C"
 	typedef void(__stdcall* OperationResultInfoCallback)(int iContext, int messageLen, const char *pMessage);
 	}
 
+bool bInicializacionGeneral = false;
+bool bInicializando = false;
+bool bOnShutdown = false;
+
+int iContextsReserved = 0;
+int iContextsActived = 0;
+
+int bad_counter = 0;
+
+typedef struct _tag_h264EncodeContext
+	{
+	bool contexto_en_uso;
+	bool contexto_con_error;
+	bool ok_critical_section;
+
+	CRITICAL_SECTION context_local_cs;
+
+	int coded_width, coded_height;
+	VideoEncoderMF *pEncoder;
+
+	int iGenericBufferLength;
+	TCHAR* pGenericBuffer;
+	} h264EncodeContext;
+
+h264EncodeContext* pH264EncodeContext = NULL;
+CRITICAL_SECTION encodeCriticalSection;
+
+#define max_number_of_contexts	16
+
+// aqui se llega con la critical section tomada
+int encoders_h264_close_forced(int iContext)
+	{
+	if (iContext >= max_number_of_contexts) return -1001;
+	if (iContext < 0) return -1002;
+	// if (bOnShutdown) return -1004;
+
+	if (pH264EncodeContext[iContext].ok_critical_section)
+		{
+		EnterCriticalSection(&pH264EncodeContext[iContext].context_local_cs);		// solo nosotros podemos usar el contexto
+		}
+	else
+		{
+		bad_counter++;
+		}
+
+	if (pH264EncodeContext[iContext].ok_critical_section)
+		{
+		LeaveCriticalSection(&pH264EncodeContext[iContext].context_local_cs);
+		DeleteCriticalSection(&pH264EncodeContext[iContext].context_local_cs);
+		}
+
+	// todo a zero, ya se puede usar el contexto
+	ZeroMemory(&pH264EncodeContext[iContext], sizeof(h264EncodeContext));
+	return 0;
+	}
+
+
+static void UnloadAndClear()
+	{
+	// primero borramos esto ya que puede estar usando recursos del contexto h264
+	bOnShutdown = true;					// signaling this is the end
+	EnterCriticalSection(&encodeCriticalSection);	// no more codec's
+
+	for (int x = 0; x < max_number_of_contexts; x++)
+		{
+		encoders_h264_close_forced(x);
+		}
+
+	// delete pContextsH264;
+	free(pH264EncodeContext);
+	pH264EncodeContext = NULL;
+
+	LeaveCriticalSection(&encodeCriticalSection);
+	DeleteCriticalSection(&encodeCriticalSection);
+	}
+
+void InicializarContextosH264()
+	{
+	bInicializando = true;
+	if (!bInicializacionGeneral)
+		{
+		InitializeCriticalSection(&encodeCriticalSection);
+		EnterCriticalSection(&encodeCriticalSection);
+
+		pH264EncodeContext = (h264EncodeContext*)malloc(max_number_of_contexts * sizeof(h264EncodeContext));
+
+		ZeroMemory(pH264EncodeContext, sizeof(h264EncodeContext) * max_number_of_contexts);
+		iContextsReserved = max_number_of_contexts;
+
+		// preparamos la salida
+		atexit(UnloadAndClear);
+		bInicializacionGeneral = true;
+
+		LeaveCriticalSection(&encodeCriticalSection);
+		}
+
+	bInicializando = false;
+	}
+
+
 // encode_h264_init inicializar el compresor h264
 ENCODERH264_API int encode_h264_init(int iWidth, int iHeight, int modo, long long opResultCallback)
 	{
+	if (bInicializando) while (bInicializando) Sleep(10);
+	if (!bInicializacionGeneral) InicializarContextosH264();
+
+	EnterCriticalSection(&encodeCriticalSection);
+
+	int ret_val = -1000;
+	for (int x = 0; x < max_number_of_contexts; x++)
+		{
+		if (!pH264EncodeContext[x].contexto_en_uso)
+			{
+			if (bOnShutdown)
+				{
+				LeaveCriticalSection(&encodeCriticalSection);
+				return -1004;
+				}
+
+			// eliminamos cualquier posible basura pre-existente
+			ZeroMemory(&pH264EncodeContext[x], sizeof(h264EncodeContext));
+
+			// indicamos en uso...
+			pH264EncodeContext[x].contexto_en_uso = true;
+
+			// inicializa la critical section de la instancia
+			InitializeCriticalSection(&pH264EncodeContext[x].context_local_cs);
+			EnterCriticalSection(&pH264EncodeContext[x].context_local_cs);
+			pH264EncodeContext[x].ok_critical_section = true;
+
+			// libera la critical section global
+			LeaveCriticalSection(&encodeCriticalSection);
+
+			// -----------------------------------
+			pH264EncodeContext[x].coded_width = iWidth;
+			pH264EncodeContext[x].coded_height = iHeight;
+
+			std::array<unsigned short, 2> dims = { static_cast<unsigned short>(pH264EncodeContext[x].coded_width), static_cast<unsigned short>(pH264EncodeContext[x].coded_height) };
+			pH264EncodeContext[x].pEncoder = new VideoEncoderMF(dims, 25);
+
+			ret_val = x;
+			LeaveCriticalSection(&pH264EncodeContext[x].context_local_cs);
+
+			if (opResultCallback > 0)
+				{
+				std::stringstream  ss;
+				ss << "{encode_h264_init} ok init context. id: " << ret_val;
+				((OperationResultInfoCallback)opResultCallback)(0, ss.str().length(), ss.str().c_str());
+				}
+
+			return ret_val;
+			}
+		}
+
+	LeaveCriticalSection(&encodeCriticalSection);
+	if (opResultCallback > 0)
+		{
+		std::stringstream  ss;
+		ss << "{encode_h264_init} Error al buscar un contexto disponible (operacion no realizada). errCode: " << ret_val;
+		((OperationResultInfoCallback)opResultCallback)(0, ss.str().length(), ss.str().c_str());
+		}
+	return ret_val;
+
 	if (opResultCallback > 0)
 		{
 		//TCHAR mensaje[1024];
@@ -67,6 +229,22 @@ ENCODERH264_API int encode_frame_from_rgb32_to_h264(int iContext, int frameNum, 
 // encode_h264_close close and free compresor h264
 ENCODERH264_API int encode_h264_close(int iContext, long long opResultCallback)
 	{
+	if (iContext >= 0)
+		{
+		// lockea el uso del contexto
+		EnterCriticalSection(&pH264EncodeContext[iContext].context_local_cs);
+		encoders_h264_close_forced(iContext);
+
+		if (opResultCallback > 0)
+			{
+			std::stringstream  ss;
+			ss << "{encode_h264_close} ok close the encoder context. id: " << iContext;
+			((OperationResultInfoCallback)opResultCallback)(0, ss.str().length(), ss.str().c_str());
+			}
+
+		return 0;
+		}
+
 	if (opResultCallback > 0)
 		{
 		//TCHAR mensaje[1024];
--- EncoderH264/EncoderH264.h
+++ EncoderH264/EncoderH264.h
@@ -1,3 +1,5 @@
+#pragma once
+
 // The following ifdef block is the standard way of creating macros which make exporting
 // from a DLL simpler. All files within this DLL are compiled with the ENCODERH264_EXPORTS
 // symbol defined on the command line. This symbol should not be defined on any project
@@ -15,7 +17,7 @@ class ENCODERH264_API CEncoderH264 {
 public:
 	CEncoderH264(void);
 	// TODO: add your methods here.
-};
+	};
 
 extern ENCODERH264_API int nEncoderH264;
 
@@ -24,4 +26,315 @@ ENCODERH264_API int fnEncoderH264(void);
 #ifndef byte
 #define	byte	BYTE
 #endif
+
+// https://github.com/forderud/AppWebStream/blob/master/VideoEncoder.hpp
+#include <stdexcept>
+#include <iostream>
+#include <vector>
+#include <array>
+#include <cassert>
+#include <Windows.h>
+#include <mfapi.h>
+#include <atlbase.h>
+
+/** 32bit color value. */
+struct R8G8B8A8 {
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+	unsigned char a;
+	};
+
+#include <mfidl.h>
+#include <Mfreadwrite.h>
+#include <Ks.h>
+#include <Codecapi.h>
+#include <Dshow.h>
+#include <mferror.h>
+#include <comdef.h>  // COM smart-ptr with "Ptr" suffix
+
+#pragma comment(lib, "mf.lib")
+#pragma comment(lib, "mfreadwrite.lib")
+#pragma comment(lib, "mfplat.lib")
+#pragma comment(lib, "mfuuid.lib")
+#pragma comment(lib, "Strmiids.lib")
+
+
+_COM_SMARTPTR_TYPEDEF(IMFSinkWriter, __uuidof(IMFSinkWriter));
+_COM_SMARTPTR_TYPEDEF(IMFMediaBuffer, __uuidof(IMFMediaBuffer));
+_COM_SMARTPTR_TYPEDEF(IMFSample, __uuidof(IMFSample));
+_COM_SMARTPTR_TYPEDEF(IMFMediaType, __uuidof(IMFMediaType));
+
+/** Converts unicode string to ASCII */
+static inline std::string ToAscii(const std::wstring& w_str) {
+#pragma warning(push)
+#pragma warning(disable: 4996) // function or variable may be unsafe
+	size_t N = w_str.size();
+	std::string s_str;
+	s_str.resize(N);
+	wcstombs(const_cast<char*>(s_str.data()), w_str.c_str(), N);
+
+	return s_str;
+#pragma warning(pop)
+	}
+
+// virtual encoder
+class VideoEncoder
+	{
+	public:
+		/** Grow size to become a multiple of the MEPG macroblock size (typ. 8). */
+		static unsigned int Align(unsigned int size, unsigned int block_size = 8)
+			{
+			if ((size % block_size) == 0)
+				return size;
+			else
+				return size + block_size - (size % block_size);
+			}
+
+		VideoEncoder(std::array<unsigned short, 2> dimensions) : m_width(dimensions[0]), m_height(dimensions[1])
+			{
+			}
+
+		virtual ~VideoEncoder() = default;
+
+		std::array<unsigned short, 2> Dims() const
+			{
+			return { m_width, m_height };
+			}
+
+		virtual R8G8B8A8* WriteFrameBegin() = 0;
+		virtual HRESULT   WriteFrameEnd() = 0;
+
+		HRESULT WriteFrame(R8G8B8A8* src_data, bool swap_rb)
+			{
+			R8G8B8A8* buffer_ptr = WriteFrameBegin();
+
+			for (unsigned int j = 0; j < m_height; j++) {
+				R8G8B8A8* src_row = &src_data[(m_height - 1 - j) * m_width]; // flip upside down
+				R8G8B8A8* dst_row = &buffer_ptr[j * Align(m_width)];
+
+				if (swap_rb)
+					{
+					for (unsigned int i = 0; i < m_width; i++)
+						dst_row[i] = SwapRGBAtoBGRA(src_row[i]);
+					}
+				else
+					{
+					// copy scanline as-is
+					memcpy(dst_row, src_row, 4 * m_width);
+					}
+
+				// clear padding at end of scanline
+				size_t hor_padding = Align(m_width) - m_width;
+				if (hor_padding)
+					std::memset(&dst_row[m_width], 0, 4 * hor_padding);
+				}
+
+			// clear padding after last scanline
+			size_t vert_padding = Align(m_height) - m_height;
+			if (vert_padding)
+				std::memset(&buffer_ptr[m_height * Align(m_width)], 0, 4 * Align(m_width) * vert_padding);
+
+			return WriteFrameEnd();
+			}
+
+		static R8G8B8A8 SwapRGBAtoBGRA(R8G8B8A8 in)
+			{
+			return{ in.b, in.g, in.r, in.a };
+			}
+
+	protected:
+		const unsigned short m_width;  ///< horizontal img. resolution (excluding padding)
+		const unsigned short m_height; ///< vertical img. resolution (excluding padding)
+	};
+
+
+/** Media-Foundation-based H.264 video encoder. */
+class VideoEncoderMF : public VideoEncoder {
+public:
+	// ** File-based video encoding. **
+	VideoEncoderMF(std::array<unsigned short, 2> dimensions, unsigned int fps, const wchar_t* filename)
+		: VideoEncoderMF(dimensions, fps)
+		{
+		const unsigned int bit_rate = static_cast<unsigned int>(0.78f * fps * Align(m_width) * Align(m_height)); // yields 40Mb/s for 1920x1080@25fps (max blu-ray quality)
+
+		CComPtr<IMFAttributes> attribs;
+		COM_CHECK(MFCreateAttributes(&attribs, 0));
+		COM_CHECK(attribs->SetGUID(MF_TRANSCODE_CONTAINERTYPE, MFTranscodeContainerType_MPEG4));
+		COM_CHECK(attribs->SetUINT32(MF_READWRITE_ENABLE_HARDWARE_TRANSFORMS, TRUE));
+
+		// create sink writer with specified output format
+		COM_CHECK(MFCreateSinkWriterFromURL(filename, nullptr, attribs, &m_sink_writer));
+		IMFMediaTypePtr mediaTypeOut = MediaTypeutput(fps, bit_rate);
+		COM_CHECK(m_sink_writer->AddStream(mediaTypeOut, &m_stream_index));
+
+		// connect input to output
+		IMFMediaTypePtr mediaTypeIn = MediaTypeInput(fps);
+		COM_CHECK(m_sink_writer->SetInputMediaType(m_stream_index, mediaTypeIn, nullptr));
+		COM_CHECK(m_sink_writer->BeginWriting());
+		}
+
+	// ** Stream-based video encoding. The underlying MFCreateFMPEG4MediaSink system call require Windows 8 or newer. **
+	VideoEncoderMF(std::array<unsigned short, 2> dimensions, unsigned int fps, IMFByteStream* stream)
+		: VideoEncoderMF(dimensions, fps)
+		{
+		const unsigned int bit_rate = static_cast<unsigned int>(0.78f * fps * m_width * m_height); // yields 40Mb/s for 1920x1080@25fps
+
+		CComPtr<IMFAttributes> attribs;
+		COM_CHECK(MFCreateAttributes(&attribs, 0));
+		COM_CHECK(attribs->SetGUID(MF_TRANSCODE_CONTAINERTYPE, MFTranscodeContainerType_FMPEG4));
+		COM_CHECK(attribs->SetUINT32(MF_LOW_LATENCY, TRUE));
+		COM_CHECK(attribs->SetUINT32(MF_READWRITE_ENABLE_HARDWARE_TRANSFORMS, TRUE));
+
+		// create sink writer with specified output format
+		IMFMediaTypePtr mediaTypeOut = MediaTypeutput(fps, bit_rate);
+		COM_CHECK(MFCreateFMPEG4MediaSink(stream, mediaTypeOut, nullptr, &m_media_sink)); // "fragmented" MPEG4 does not require seekable byte-stream
+		COM_CHECK(MFCreateSinkWriterFromMediaSink(m_media_sink, attribs, &m_sink_writer));
+
+		// connect input to output
+		IMFMediaTypePtr mediaTypeIn = MediaTypeInput(fps);
+		COM_CHECK(m_sink_writer->SetInputMediaType(m_stream_index, mediaTypeIn, nullptr));
+
+		{
+			// access H.264 encoder directly (https://msdn.microsoft.com/en-us/library/windows/desktop/dd797816.aspx)
+		CComPtr<ICodecAPI> codec;
+		COM_CHECK(m_sink_writer->GetServiceForStream(m_stream_index, GUID_NULL, IID_ICodecAPI, (void**)&codec));
+		CComVariant quality;
+		codec->GetValue(&CODECAPI_AVEncCommonQuality, &quality); // not supported by Intel encoder (mfx_mft_h264ve_64.dll)
+		CComVariant low_latency;
+		COM_CHECK(codec->GetValue(&CODECAPI_AVLowLatencyMode, &low_latency));
+		//assert(low_latency.boolVal != FALSE);
+		// CODECAPI_AVEncAdaptiveMode not implemented
+
+		// query group-of-pictures (GoP) size
+		CComVariant gop_size;
+		COM_CHECK(codec->GetValue(&CODECAPI_AVEncMPVGOPSize, &gop_size));
+		//gop_size = (unsigned int)1; // VT_UI4 type
+		//COM_CHECK(codec->SetValue(&CODECAPI_AVEncMPVGOPSize, &gop_size));
+		}
+
+		COM_CHECK(m_sink_writer->BeginWriting());
+		}
+
+	// common constructor
+	VideoEncoderMF(std::array<unsigned short, 2> dimensions, unsigned int fps) : VideoEncoder(dimensions)
+		{
+		COM_CHECK(MFStartup(MF_VERSION));
+		COM_CHECK(MFFrameRateToAverageTimePerFrame(fps, 1, const_cast<unsigned long long*>(&m_frame_duration)));
+		}
+
+	~VideoEncoderMF() noexcept
+		{
+		HRESULT hr = m_sink_writer->Finalize(); // fails on prior I/O errors
+		hr; // discard error
+
+		// delete objects before shutdown-call
+		m_buffer.Release();
+		m_sink_writer.Release();
+
+		if (m_media_sink)
+			{
+			COM_CHECK(m_media_sink->Shutdown());
+			m_media_sink.Release();
+			}
+
+		COM_CHECK(MFShutdown());
+		}
+
+	R8G8B8A8* WriteFrameBegin() override
+		{
+		const DWORD frame_size = 4 * Align(m_width) * Align(m_height);
+
+		// Create a new memory buffer.
+		if (!m_buffer)
+			COM_CHECK(MFCreateMemoryBuffer(frame_size, &m_buffer));
+
+		// Lock buffer to get data pointer
+		R8G8B8A8* buffer_ptr = nullptr;
+		COM_CHECK(m_buffer->Lock(reinterpret_cast<BYTE**>(&buffer_ptr), NULL, NULL));
+		return buffer_ptr;
+		}
+
+	HRESULT WriteFrameEnd() override
+		{
+		const DWORD frame_size = 4 * Align(m_width) * Align(m_height);
+
+		COM_CHECK(m_buffer->Unlock());
+
+		// Set the data length of the buffer.
+		COM_CHECK(m_buffer->SetCurrentLength(frame_size));
+
+		// Create a media sample and add the buffer to the sample.
+		IMFSamplePtr sample;
+		COM_CHECK(MFCreateSample(&sample));
+		COM_CHECK(sample->AddBuffer(m_buffer));
+
+		// Set the time stamp and the duration.
+		COM_CHECK(sample->SetSampleTime(m_time_stamp));
+		COM_CHECK(sample->SetSampleDuration(m_frame_duration));
+
+		// send sample to Sink Writer.
+		HRESULT hr = m_sink_writer->WriteSample(m_stream_index, sample); // fails on I/O error
+		if (FAILED(hr))
+			return hr;
+
+		// increment time
+		m_time_stamp += m_frame_duration;
+		return S_OK;
+		}
+
+private:
+	IMFMediaTypePtr MediaTypeInput(unsigned int fps)
+		{
+		// configure input format. Frame size is aligned to avoid crash
+		IMFMediaTypePtr mediaTypeIn;
+		COM_CHECK(MFCreateMediaType(&mediaTypeIn));
+		COM_CHECK(mediaTypeIn->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video));
+		COM_CHECK(mediaTypeIn->SetGUID(MF_MT_SUBTYPE, MFVideoFormat_RGB32)); // X8R8G8B8 format
+		COM_CHECK(mediaTypeIn->SetUINT32(MF_MT_INTERLACE_MODE, MFVideoInterlace_Progressive));
+		COM_CHECK(mediaTypeIn->SetUINT32(MF_MT_ALL_SAMPLES_INDEPENDENT, TRUE));
+		// Frame size is aligned to avoid crash
+		COM_CHECK(MFSetAttributeSize(mediaTypeIn, MF_MT_FRAME_SIZE, Align(m_width), Align(m_height)));
+		COM_CHECK(MFSetAttributeRatio(mediaTypeIn, MF_MT_FRAME_RATE, fps, 1));
+		COM_CHECK(MFSetAttributeRatio(mediaTypeIn, MF_MT_PIXEL_ASPECT_RATIO, 1, 1));
+		return mediaTypeIn;
+		}
+
+	IMFMediaTypePtr MediaTypeutput(unsigned int fps, unsigned int bit_rate)
+		{
+		IMFMediaTypePtr mediaTypeOut;
+		COM_CHECK(MFCreateMediaType(&mediaTypeOut));
+		COM_CHECK(mediaTypeOut->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video));
+		COM_CHECK(mediaTypeOut->SetGUID(MF_MT_SUBTYPE, MFVideoFormat_H264)); // H.264 format
+		COM_CHECK(mediaTypeOut->SetUINT32(MF_MT_AVG_BITRATE, bit_rate));
+		COM_CHECK(mediaTypeOut->SetUINT32(MF_MT_INTERLACE_MODE, MFVideoInterlace_Progressive));
+		// Frame size is aligned to avoid crash
+		COM_CHECK(MFSetAttributeSize(mediaTypeOut, MF_MT_FRAME_SIZE, Align(m_width), Align(m_height)));
+		COM_CHECK(MFSetAttributeRatio(mediaTypeOut, MF_MT_FRAME_RATE, fps, 1));
+		COM_CHECK(MFSetAttributeRatio(mediaTypeOut, MF_MT_PIXEL_ASPECT_RATIO, 1, 1));
+		return mediaTypeOut;
+		}
+
+	static void COM_CHECK(HRESULT hr)
+		{
+		if (FAILED(hr)) {
+			_com_error err(hr);
+#ifdef _UNICODE
+			const wchar_t* msg = err.ErrorMessage(); // weak ptr.
+			throw std::runtime_error(ToAscii(msg));
+#else
+			const char* msg = err.ErrorMessage(); // weak ptr.
+			throw std::runtime_error(msg);
+#endif
+			}
+		}
+
+	const unsigned long long m_frame_duration = 0;
+	long long                m_time_stamp = 0;
+
+	CComPtr<IMFMediaSink>    m_media_sink;
+	IMFSinkWriterPtr         m_sink_writer;
+	IMFMediaBufferPtr        m_buffer;
+	unsigned long            m_stream_index = 0;
+	};
 
